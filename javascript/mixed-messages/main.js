0b3270dcf3f8907d299e0a41703bddd029cd6388
Danilo Vitelli Camillo <vitelli.camillo@gmail.com>
2021-04-13 20:31:18 -0300

    First commit

---
+++ javascript/mixed-messages/main.js
@@ -0,0 +1,13 @@
+/*Program to log mixed messages in the console
+author Danilo Vitelli Camillo
+*/
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*365);
+}
+
+const collectiveWisdow = {
+    
+}
+
+console.log(getRandomNumber());
